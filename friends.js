c8fee71097ffa68900c7b90026e32fd8b5bda3d6
Quynh Luu <quynhtlluu@gmail.com>
2022-08-04 15:07:27 -0700

    Add conditional

--- friends.js
+++ friends.js
@@ -2,5 +2,8 @@ var numbers = [7, 24, 29, 35, 61, 72, 81, 96]
 for (var i = 0; i < 30; i++) {
   if (numbers[i] > 30) {
     console.log(numbers[i])
+  } else if (numbers[i] < 60) {
+    console.log(numbers[i])
+    
   }
 }
